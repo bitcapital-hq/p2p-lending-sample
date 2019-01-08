0c9e9980f561fa1ae0ba8a3ecf646a069e1b3b90
Fabio Dias Soares <fabio.dias@bitcapital.com.br>
2019-01-08 16:16:20 -0200

    Add IsCPF to user model

--- api/models/User.ts
+++ api/models/User.ts
@@ -1,5 +1,22 @@
-import { IsAlphanumeric, IsEmail, validate } from "class-validator";
+import { Validate, IsAlphanumeric, ValidatorConstraint, ValidationArguments, ValidatorConstraintInterface, IsEmail, validate, IsBoolean, IsInt } from "class-validator";
 import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import * as cpf from "cpf";
+
+export enum UserStatus {
+  ACTIVE = "active",
+  PENDING = "pending",
+  INACTIVE = "inactive"
+}
+
+class IsCPF implements ValidatorConstraintInterface {
+  validate(text: string) {
+    return cpf.isValid(text);
+  }
+
+  defaultMessage(args: ValidationArguments) {
+    return "Value ($value) is not a valid CPF.";
+  }
+}
 
 @Entity(User.tableName)
 export default class User extends BaseEntity {
@@ -20,6 +37,38 @@ export default class User extends BaseEntity {
   @Column({ nullable: false, unique: true })
   email: string;
 
+  @IsInt()
+  @Column({ nullable: true, type: 'int',  unique: true })
+  phoneNumber: number;
+  
+  @IsAlphanumeric()
+  @Column({ nullable: true, unique: true })
+  phoneCountryCode: string;
+
+  @IsAlphanumeric()
+  @Column({ nullable: true, type: 'int', unique: true })
+  phoneLocalCode: number;
+
+  @Validate(IsCPF)
+  @Column({ nullable: true })
+  finacialId: string
+
+  @IsBoolean()
+  @Column({ nullable: false, default: false })
+  status: boolean  
+
+  @IsAlphanumeric()
+  @Column({ nullable: true })
+  address: string
+
+  @IsAlphanumeric()
+  @Column({ nullable: true })
+  addressNumber: string
+
+  @IsAlphanumeric()
+  @Column({ nullable: true })
+  addressComplement: string
+
   constructor(data: Partial<User>) {
     super();
     Object.assign(this, data, {});
@@ -38,3 +87,22 @@ export default class User extends BaseEntity {
     return this.findOne({ where: { email } });
   }
 }
+
+/**
+ Usuário
+Telefone
+Country Code
+Code
+Number
+Endereço
+Logradouro
+Número
+CEP
+Complemento
+Referência
+Dados bancários para saque (Opcional)
+Nr da Agência
+Nr da Conta
+Código do Banco
+
+ */
