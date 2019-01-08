b5d16bb83e37182e4ebf211978ecc5698be544f9
Fabio Dias Soares <fabio.dias@bitcapital.com.br>
2019-01-08 15:21:32 -0200

    First commit - few user model changes

--- config/database.config.ts
+++ config/database.config.ts
@@ -7,9 +7,9 @@ export default {
   synchronize: false,
   host: process.env.DATABASE_HOST || "localhost",
   port: process.env.DATABASE_PORT || "5432",
-  username: process.env.DATABASE_USER || "postgres",
-  password: process.env.DATABASE_PASSWORD || "postgres",
-  database: process.env.DATABASE_NAME || "p2p-lending-sample",
+  username: process.env.DATABASE_USER || "apps",
+  password: process.env.DATABASE_PASSWORD || "bitcapital",
+  database: process.env.DATABASE_NAME || "lending",
 
   // IMPORTANT: Path should be relative to root
   entities: ["./api/models/**/*.ts"],
