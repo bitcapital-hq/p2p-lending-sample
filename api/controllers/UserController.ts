bc95fb5f2f835b8cce3abdec6eedc0c25f03edc4
Fabio Dias Soares <fabio.dias@bitcapital.com.br>
2019-01-13 20:02:08 -0200

    [wip] - error handler wip

--- api/controllers/UserController.ts
+++ api/controllers/UserController.ts
@@ -3,23 +3,24 @@ import BitcapitalService from '../services/BitcapitalService' ;
 
 @Controller("/users")
 export default class UserController {
-    /**
-     * GET /users
-     * @description List users
-     */
-    @Get('/')
-    static async getUsers(req: BaseRequest, res: BaseResponse) {
-      console.log('AFTERAFTERAFTERAFTERAFTERAFTERAFTERAFTERAFTERAFTERAFTERAFTERAFTER')
-      let data: object = await BitcapitalService.getAPIClient();
-      return res.success(data);
+  /**
+   * GET /users
+   * @description List users
+   */
+  @Get("/")
+  static async getUsers(req: BaseRequest, res: BaseResponse) {
+    console.log('AFTERAFTERAFTERAFTERAFTERAFTERAFTERAFTERAFTERAFTERAFTERAFTERAFTER')
+    let data: object = await BitcapitalService.getAPIClient();
+    return res.success(data);
+  }
+  @Post("/authenticate")
+  static async authenticate(req: BaseRequest, res: BaseResponse) {
+    try {
+      let authenticatedUser = await BitcapitalService.authenticate(req.body.username, req.body.password);
+      res.success(authenticatedUser);
+    } catch(e) {
+      let status = e.message.includes('with status')
+      throw new HttpError(e.message, e.status);
     }
-    @Post("/authenticate")
-    static async authenticate(req: BaseRequest, res: BaseResponse) {
-      try {
-        let authenticatedUser = await BitcapitalService.authenticate(req.body.username, req.body.password);
-        res.success(authenticatedUser);
-      } catch(e) {
-        throw new HttpError(e.message, e.status);
-      }
   }
 };
